a2b8b1abe33a79653447047ab37ba1baece595e3
Eugene Umbras <umbras2000@gmail.com>
2020-05-20 09:43:09 +0300

    translation vectors added

--- headpose_estimator.py
+++ headpose_estimator.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 
 class HeadPoseEstimator:
@@ -60,6 +61,32 @@ class HeadPoseEstimator:
 
         return (rotation_vector, translation_vector)
 
+    def get_vectors(self, rotation_vector, translation_vector):
+        axis = np.float32([[500, 0, 0],
+                           [0, 500, 0],
+                           [0, 0, 500]])
+
+        imgpts, jac = cv2.projectPoints(
+            axis, rotation_vector, translation_vector, self.camera_matrix, self.dist_coeefs)
+        modelpts, jac2 = cv2.projectPoints(
+            self.points_3d, rotation_vector, translation_vector, self.camera_matrix, self.dist_coeefs)
+        rvec_matrix = cv2.Rodrigues(rotation_vector)[0]
+
+        proj_matrix = np.hstack((rvec_matrix, translation_vector))
+        eulerAngles = cv2.decomposeProjectionMatrix(proj_matrix)[6]
+
+        pitch, yaw, roll = [math.radians(_) for _ in eulerAngles]
+
+        pitch = math.degrees(math.asin(math.sin(pitch)))
+        roll = -math.degrees(math.asin(math.sin(roll)))
+        yaw = math.degrees(math.asin(math.sin(yaw)))
+
+        self.pitch = pitch
+        self.roll = roll
+        self.yaw = yaw
+
+        return (str(int(roll)), str(int(pitch)), str(int(yaw)))
+
     def draw_face_direction(self, image, nose, rotation_vector, translation_vector, color=(255, 0, 0), line_width=2):
         (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 200.0)]), rotation_vector,
                                                          translation_vector, self.camera_matrix, self.dist_coeefs)
@@ -69,7 +96,153 @@ class HeadPoseEstimator:
 
         cv2.line(image, p1, p2, color, line_width)
 
-    def draw_all_landmarks(self, image, marks):
-        marks = marks.astype(int)
-        for mark in marks:
-            cv2.circle(image, tuple(mark), 2, (71, 99, 255), 1)
+    def draw_all_landmarks(self, image, landmarks):
+        landmarks = landmarks.astype(int)
+        for landmark in landmarks:
+            cv2.circle(image, tuple(landmark), 2, (71, 99, 255), 1)
+
+    def draw_mask(self, image, landmarks):
+        landmarks = landmarks.astype(int)
+        for i in range(16):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[0]), tuple(
+            landmarks[17]-[0, 35]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[16]), tuple(
+            landmarks[26]-[0, 35]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[17]-[0, 35]),
+                 tuple(landmarks[18]-[0, 35]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[26]-[0, 35]),
+                 tuple(landmarks[25]-[0, 35]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[18]-[0, 35]),
+                 tuple(landmarks[19]-[0, 35]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[25]-[0, 35]),
+                 tuple(landmarks[24]-[0, 35]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[19]-[0, 35]),
+                 tuple(landmarks[20]-[0, 40]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[24]-[0, 35]),
+                 tuple(landmarks[23]-[0, 40]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[20]-[0, 40]),
+                 tuple(landmarks[21]-[0, 47]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[23]-[0, 40]),
+                 tuple(landmarks[22]-[0, 47]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[21]-[0, 47]),
+                 tuple(landmarks[22]-[0, 47]), (221, 226, 179), 1)
+
+        for i in range(17, 21):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+
+        for i in range(22, 26):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+
+        for i in range(36, 41):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[41]), tuple(
+            landmarks[36]), (221, 226, 179), 1)
+
+        for i in range(42, 47):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[47]), tuple(
+            landmarks[42]), (221, 226, 179), 1)
+
+        for i in range(27, 30):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+
+        for i in range(31, 35):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+
+        for i in range(48, 59):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[59]), tuple(
+            landmarks[48]), (221, 226, 179), 1)
+
+        for i in range(60, 67):
+            cv2.line(image, tuple(landmarks[i]), tuple(
+                landmarks[i+1]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[67]), tuple(
+            landmarks[60]), (221, 226, 179), 1)
+
+        lm = [1, 15, 8, 1]
+        for i in range(3):
+            cv2.circle(image, tuple(landmarks[lm[i]]), 3, (0, 0, 255), 1)
+            cv2.line(image, tuple(landmarks[lm[i]]), tuple(
+                landmarks[lm[i+1]]), (0, 0, 150), 1)
+
+        cv2.line(image, tuple(landmarks[31]), tuple(
+            landmarks[48]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[35]), tuple(
+            landmarks[54]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[35]), tuple(
+            landmarks[15]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[45]), tuple(
+            landmarks[15]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[31]), tuple(
+            landmarks[1]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[36]), tuple(
+            landmarks[1]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[39]), tuple(
+            landmarks[31]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[35]), tuple(
+            landmarks[42]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[33]), tuple(
+            landmarks[50]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[33]), tuple(
+            landmarks[52]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[5]), tuple(
+            landmarks[48]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[11]), tuple(
+            landmarks[54]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[8]), tuple(
+            landmarks[57]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[8]), tuple(
+            landmarks[48]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[8]), tuple(
+            landmarks[54]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[54]), tuple(
+            landmarks[14]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[35]), tuple(
+            landmarks[14]), (221, 226, 179), 1)
+
+        cv2.line(image, tuple(landmarks[31]), tuple(
+            landmarks[2]), (221, 226, 179), 1)
+        cv2.line(image, tuple(landmarks[48]), tuple(
+            landmarks[2]), (221, 226, 179), 1)
+
+    def draw_vectors(self, image):
+        label_roll = "roll: %.4f" % self.roll
+        label_size, base_line = cv2.getTextSize(
+            label_roll, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        cv2.putText(image, label_roll, (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+        label_pitch = "pitch: %.4f" % self.pitch
+        label_size, base_line = cv2.getTextSize(
+            label_pitch, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        cv2.putText(image, label_pitch, (10, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        label_yaw = "yaw: %.4f" % self.yaw
+        label_size, base_line = cv2.getTextSize(
+            label_yaw, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        cv2.putText(image, label_yaw, (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
